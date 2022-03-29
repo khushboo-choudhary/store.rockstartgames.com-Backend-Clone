1049b3020453fb9a1ec503f6aa7920aa893d502e
aman-638 <ar7009297@gmail.com>
2022-03-29 15:38:15 +0530

    db

--- src/confige/db.js
+++ src/confige/db.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const mongoose=require("mongoose");
 const connect = () => {
     return mongoose.connect(
-       "mongodb+srv://aman_638:aman_638@cluster0.txhrb.mongodb.net/rockstartgames?retryWrites=true&w=majority"
+       process.env.mongoDB
     )
 }
 
