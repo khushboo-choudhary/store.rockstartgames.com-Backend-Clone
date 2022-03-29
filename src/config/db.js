3cff875fa562491df98ff0f318cf6cb5dc6b4ab2
aman-638 <ar7009297@gmail.com>
2022-03-29 22:14:12 +0530

    name changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose=require("mongoose");
 const connect = () => {
     return mongoose.connect(
-       process.env.mongoDB
+       process.env.MONGODB_URL
     )
 }
 
