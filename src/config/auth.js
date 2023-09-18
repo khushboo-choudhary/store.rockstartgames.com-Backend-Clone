9977947ab1c85b0f88451c376e9c85f1bf7ab60e
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-09-18 15:19:59 +0530

    Deployment url changed.

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 const User = require("../models/Users.model");
 var GoogleStrategy = require("passport-google-oauth2").Strategy;
 const passport = require("passport");
-const {v4} = require("uuid");
+const { v4 } = require("uuid");
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-        "https://obscure-citadel-15133.herokuapp.com/google/callback",
+      callbackURL: "https://clear-pear-tuna.cyclic.app/google/callback",
     },
     async function (request, accessToken, refreshToken, profile, done) {
       let user = await User.findOne({ email: profile?.email }).lean().exec();
